eb79ce4c7a160de4eb7fd3fb0112e5c319423244
Shibasis Patnaik <shibasis0801@gmail.com>
2020-04-05 17:16:42 +0530

    Started writing basic component framework

---
+++ app/src/main/java/com/phoenixoverlord/pravegaapp/ExtendedLifecycle.kt
@@ -0,0 +1,53 @@
+package com.phoenixoverlord.pravegaapp
+
+import android.content.Intent
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+
+interface Component: DefaultLifecycleObserver {
+    fun onActivityResult(owner: LifecycleOwner, requestCode: Int, resultCode: Int, data: Intent?) {}
+    fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {}
+}
+
+class test: Component {
+    override fun onActivityResult(
+        owner: LifecycleOwner,
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(owner, requestCode, resultCode, data)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onCreate(owner: LifecycleOwner) {
+        super.onCreate(owner)
+    }
+
+    override fun onResume(owner: LifecycleOwner) {
+        super.onResume(owner)
+    }
+
+    override fun onPause(owner: LifecycleOwner) {
+        super.onPause(owner)
+    }
+
+    override fun onStart(owner: LifecycleOwner) {
+        super.onStart(owner)
+    }
+
+    override fun onStop(owner: LifecycleOwner) {
+        super.onStop(owner)
+    }
+
+    override fun onDestroy(owner: LifecycleOwner) {
+        super.onDestroy(owner)
+    }
+}
