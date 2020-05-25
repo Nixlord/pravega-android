3110dc344527a7835d29184b74db869377d0ae85
Shibasis Patnaik <shibasis0801@gmail.com>
2020-05-25 16:04:45 +0530

    also working for okhttp, may just use that

--- app/src/main/java/com/phoenixoverlord/pravegaapp/MainActivity.kt
+++ app/src/main/java/com/phoenixoverlord/pravegaapp/MainActivity.kt
@@ -15,11 +15,19 @@ import com.phoenixoverlord.pravega.extensions.logDebug
 import com.phoenixoverlord.pravega.extensions.logError
 import com.phoenixoverlord.pravega.toast
 import com.phoenixoverlord.pravega.views.recyclerview.PravegaAdapter
+import com.squareup.okhttp.OkHttpClient
 import io.crossbar.autobahn.websocket.WebSocketConnection
 import io.crossbar.autobahn.websocket.WebSocketConnectionHandler
 import io.crossbar.autobahn.websocket.types.ConnectionResponse
+import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.list_item.view.*
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.WebSocket
+import okhttp3.WebSocketListener
+import okio.ByteString
+import okio.ByteString.Companion.decodeHex
 
 
 class MainActivity : AppCompatActivity() {
@@ -28,6 +36,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var adapter: PravegaAdapter<Friend>
     private val model: MainViewModel by viewModels()
     private val ws = WebSocketConnection()
+    // See why there are two okhttp, maybe one is from retrofit
+    private val http = okhttp3.OkHttpClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +47,7 @@ class MainActivity : AppCompatActivity() {
         testingWebsocketsJavax()
         testRemoteConfig()
         testingWebSocketsAutoBahn()
+        testingOkHttpWS()
 
         mainFab.setOnClickListener {
             toast("Calling ${PravegaConfig.DEV}")
@@ -49,6 +60,39 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
         }
+    }
+
+    private fun testingOkHttpWS() {
+        val request = Request.Builder().url("wss://echo.websocket.org").build()
+        http.newWebSocket(request, object: WebSocketListener() {
+            override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
+                super.onClosing(webSocket, code, reason)
+                logDebug(reason)
+            }
+
+            override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+                super.onFailure(webSocket, t, response)
+                logError(t)
+            }
+
+            override fun onMessage(webSocket: WebSocket, text: String) {
+                super.onMessage(webSocket, text)
+                logDebug(text)
+            }
+
+            override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
+                super.onMessage(webSocket, bytes)
+                logDebug(bytes.hex())
+
+            }
+
+            override fun onOpen(webSocket: WebSocket, response: Response) {
+                super.onOpen(webSocket, response)
+                webSocket.send("Hello, from Shibasis !")
+                webSocket.send("What's up ?")
+                webSocket.send("cabdab".decodeHex())
+            }
+        })
     }
 
     private fun testingWebSocketsAutoBahn() {
