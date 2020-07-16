d3d1815248efc115474491537fe6b122deb4baa5
Diksha Agarwal <agarwaldiksha6596@gmail.com>
2020-07-16 23:31:50 +0530

    UI changes

--- app/src/main/java/com/phoenixoverlord/pravegaapp/chat/ChatActivity.kt
+++ app/src/main/java/com/phoenixoverlord/pravegaapp/chat/ChatActivity.kt
@@ -1,7 +1,15 @@
 package com.phoenixoverlord.pravegaapp.chat
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.phoenixoverlord.pravegaapp.R
 
 class ChatActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+
+
+    }
 }
